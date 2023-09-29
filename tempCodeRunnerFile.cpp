9fd6369da4850d01373c9f0c073e1e090ac4fe5e
M Abdul Fazal <mmohd.abdul.fazal@gmail.com>
2023-09-29 20:41:24 +0530

    temp file run

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,25 +1,46 @@
-#include<bits/stdc++.h>
+// C++ program to find LCM of First N Natural Numbers.
+#include <bits/stdc++.h>
+#define MAX 100000
 using namespace std;
 
-int secSmall(int arr[], int n)
-{   
-    int first = INT_MAX, second = INT_MAX;
-    for(int i=0; i<n; i++)
-   {
-       if(arr[i] < first)
-        {                  
-            second = first; 
-            first = arr[i]; 
-        } 
-        else if(second>arr[i])
-            second = arr[i];
-    }
-    return second;
+vector<bool> isPrime (MAX, true);
+// utility function for sieve of sieve of Eratosthenes
+
+void sieve()
+{
+	
+	for (int i = 2; i * i <= MAX; i++)
+	{
+		if (isPrime[i] == true)
+			for (int j = i*i; j<= MAX; j+=i)
+				isPrime[j] = false;
+	}
 }
 
-int main(){
+// Function to find LCM of first n Natural Numbers
+long long LCM(int n)
+{
+	long long lcm = 1;
+	int i=2;
+	while(i<=n) {
+		if(isPrime[i]){
+			int pp = i;
+			while (pp * i <= n)
+				pp = pp * i;
+			lcm *= pp;
+		}
+		i++;
+	}
+	return lcm;
+}
 
-    int arr[] = {318, 28, 57, 71, 96, 102};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout << secSmall(arr, n) << endl;
+// Driver code
+int main()
+{
+	// build sieve
+	sieve();
+	int N = 9;
+	// Function call
+	cout << LCM(N);
+	return 0;
 }
