7ed2971cfa56673d7ea2333079262670127002d6
M Abdul Fazal <mmohd.abdul.fazal@gmail.com>
2023-10-01 15:01:22 +0530

    Constellation Problem codeVita

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,46 +1,68 @@
-// C++ program to find LCM of First N Natural Numbers.
 #include <bits/stdc++.h>
-#define MAX 100000
 using namespace std;
 
-vector<bool> isPrime (MAX, true);
-// utility function for sieve of sieve of Eratosthenes
-
-void sieve()
-{
-	
-	for (int i = 2; i * i <= MAX; i++)
-	{
-		if (isPrime[i] == true)
-			for (int j = i*i; j<= MAX; j+=i)
-				isPrime[j] = false;
-	}
-}
-
-// Function to find LCM of first n Natural Numbers
-long long LCM(int n)
-{
-	long long lcm = 1;
-	int i=2;
-	while(i<=n) {
-		if(isPrime[i]){
-			int pp = i;
-			while (pp * i <= n)
-				pp = pp * i;
-			lcm *= pp;
-		}
-		i++;
-	}
-	return lcm;
-}
-
-// Driver code
 int main()
 {
-	// build sieve
-	sieve();
-	int N = 9;
-	// Function call
-	cout << LCM(N);
-	return 0;
+	int n;
+	cin >> n;
+	char co[3][n];
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cin >> co[i][j];
+	}
+	for (int i = 0; i < n - 2; i++)
+	{
+		if (co[0][i] == '#')
+		{
+			cout << "#";
+			continue;
+		}
+		if (co[0][i] == '.' && co[0][i + 1] == '*' && co[0][i + 2] == '.')
+		{
+			if (co[1][i] == '*' && co[1][i + 1] == '*' && co[1][i + 2] == '*')
+				if (co[2][i] == '*' and co[2][i + 1] == '.' and co[2][i + 2] == '*')
+					cout << "A";
+			i += 2;
+			continue;
+		}
+		if (co[0][i] == '*' and co[0][i + 1] == '*' and co[0][i + 2] == '*')
+		{
+			if (co[1][i] == '*' and co[1][i + 1] == '*' and co[1][i + 2] == '*')
+			{
+				if (co[2][i] == '*' and co[2][i + 1] == '*' and co[2][i + 2] == '*')
+				{
+					cout << "E";
+					i += 2;
+					continue;
+				}
+			}
+			else if (co[1][i] == '.' and co[1][i + 1] == '*' and co[1][i + 2] == '.')
+			{
+				if (co[2][i] == '*' and co[2][i + 1] == '*' and co[2][i + 2] == '*')
+				{
+					cout << "I";
+					i += 2;
+					continue;
+				}
+			}
+			else if (co[1][i] == '*' and co[1][i + 1] == '.' and co[1][i + 2] == '*')
+			{
+				if (co[2][i] == '*' and co[2][i + 1] == '*' and co[2][i + 2] == '*')
+				{
+					cout << "O";
+					i += 2;
+					continue;
+				}
+			}
+		}
+		if (co[0][i] == '*' and co[0][i + 1] == '.' and co[0][i + 2] == '*')
+			if (co[1][i] == '*' and co[1][i + 1] == '.' and co[1][i + 2] == '*')
+				if (co[2][i] == '*' and co[2][i + 1] == '*' and co[2][i + 2] == '*')
+				{
+					cout << "U";
+					i += 2;
+					continue;
+				}
+	}
 }
