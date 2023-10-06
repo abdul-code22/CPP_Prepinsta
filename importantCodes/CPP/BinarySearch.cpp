85ffef3bcfdb730cea357db4af04d7f11533d00c
M Abdul Fazal <mmohd.abdul.fazal@gmail.com>
2023-10-07 01:22:30 +0530

    Binary search using while loop

--- importantCodes/CPP/BinarySearch.cpp
+++ importantCodes/CPP/BinarySearch.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int BinarySearch(int *arr, int size, int key){
+    int start = 0;
+    int end = size;
+    int mid = (start+end)/2;
 
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+
+        if (arr[mid] < key)
+        {
+            start = mid+1;
+             
+        }
+        else
+        {
+            end = mid -1;   
+        }
+
+        mid = (start + end)/2; 
+    }
+    return -1;
+    
 }
 
 int main()
@@ -11,5 +36,12 @@ int main()
     int size = sizeof(arr) / sizeof(arr[0]);
     int key;
     cin >> key;
-    cout << BinarySearch(arr, size, key);
+    sort(arr, arr + size);
+    cout << "Sorted array "<<endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout <<"Index of the element entered: " << BinarySearch(arr, size, key);
 }
